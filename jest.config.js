3d77a7f739248cec3ace61d233594c5d7eeba11e
kenuete <kenuete@gmail.com>
2025-02-09 09:20:38 +1100

    re-configure the jest to support for both node and jsdom environments

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,33 @@
 export default {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-  },
-  setupFilesAfterEnv: ['./jest.setup.ts'],
-  transform: {
-    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.test.json' }],
-  },
-  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
-  coveragePathIgnorePatterns: [
-    '/node_modules/',
-    '\\.styles\\.(js|ts|tsx)$', // Ignore any .styles.js/ts/tsx files
-    // Add more patterns as needed
-  ],  
-};
+  projects: [
+    {
+        displayName: 'frontend',
+        preset: 'ts-jest',
+        testEnvironment: 'jsdom',
+        moduleNameMapper: {
+          '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+        },
+        setupFilesAfterEnv: ['./jest.setup.ts'],
+        transform: {
+          '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.test.json' }],
+        },
+        testMatch: ['**/src/**/__tests__/**/*.[jt]s?(x)', '**/src/**/?(*.)+(spec|test).[tj]s?(x)'],
+        coveragePathIgnorePatterns: [
+          '/node_modules/',
+          '\\.styles\\.(js|ts|tsx)$', // Ignore any .styles.js/ts/tsx files
+          // Add more patterns as needed
+        ],  
+      },
+    {
+      // Backend (Express) - using node for Express tests
+      displayName: 'backend',
+      preset: 'ts-jest',
+      testEnvironment: 'node', // Use Node.js environment for backend tests
+      setupFilesAfterEnv: ['./jest.setup.ts'],
+      transform: {
+        '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.test.json' }],
+      },
+      testMatch: ['**/server/**/__tests__/**/*.[jt]s?(x)', '**/server/**/?(*.)+(spec|test).[tj]s?(x)'],
+    },
+  ],
+}
